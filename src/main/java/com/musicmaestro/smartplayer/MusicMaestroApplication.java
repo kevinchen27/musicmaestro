f54f19920f6093248852911912ac0dadd132ce42
kevinchen27 <kevin.chen27@ucla.edu>
2023-06-19 12:41:45 -0700

    First commit

---
+++ src/main/java/com/musicmaestro/smartplayer/MusicMaestroApplication.java
@@ -0,0 +1,13 @@
+package com.musicmaestro.smartplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMaestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMaestroApplication.class, args);
+	}
+
+}
